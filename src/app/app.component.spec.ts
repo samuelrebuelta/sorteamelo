42195d5c788529ff7954136b6521b6c7bcedae53
Rebuelta Ruiz <srebuelta@eservicios.indra.es>
2019-05-10 09:25:10 +0200

    change project name

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -13,15 +13,15 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'pruebas'`, async(() => {
+  it(`should have as title 'sorteamelo'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('pruebas');
+    expect(app.title).toEqual('sorteamelo');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to pruebas!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to sorteamelo!');
   }));
 });
